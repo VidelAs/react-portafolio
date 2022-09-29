8e20b6039152fdef000ebe4507f152c04003266c
VidelAs <videl131103@gmail.com>
2022-09-29 16:37:55 -0600

    Update del archivo app.js, skills.js, banner.js. Creacion del componente projects.js y projesCard.js

---
+++ src/components/Projects.js
@@ -0,0 +1,64 @@
+import { Container, Row, Col, Tab, Nav } from 'react-bootstrap';
+import { ProjectCard } from './ProjectCard';
+import projImg1 from '../assets/img/project-img1.png';
+import projImg2 from '../assets/img/project-img2.png';
+import projImg3 from '../assets/img/project-img3.png';
+
+//import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+  const projects = [
+    {
+      imgUrl: projImg1,
+    },
+    {
+      imgUrl: projImg2,
+    },
+    {
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? 'animate__animated animate__fadeIn' : ''
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>
+                    Some projects that I have carried out in these courses would
+                    be these
+                  </p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? 'animate__animated animate__slideInUp' : ''
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
