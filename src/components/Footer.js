c23b673d3214f8f0dfab08949941a795db6fec90
VidelAs <videl131103@gmail.com>
2022-10-11 12:15:32 -0600

    Changes Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,17 +14,17 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#">
-                <img src={navIcon1} alt="Icon" />
+              <a href="https://www.linkedin.com/in/videl-andrea-araya-sanchez-b4a914239">
+                <img src={navIcon1} alt="" />
               </a>
-              <a href="#">
-                <img src={navIcon2} alt="Icon" />
+              <a href="https://www.facebook.com/videl.arayasanchez/">
+                <img src={navIcon2} alt="" />
               </a>
-              <a href="#">
-                <img src={navIcon3} alt="Icon" />
+              <a href="https://www.instagram.com/videl03/">
+                <img src={navIcon3} alt="" />
               </a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Videl Araya 2022.</p>
           </Col>
         </Row>
       </Container>
